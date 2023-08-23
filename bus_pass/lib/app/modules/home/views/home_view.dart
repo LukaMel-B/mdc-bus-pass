33afc78dfab15c61845872df137c2ca446ecccdb
Luha Saniyya <luluhsany@sscollege.ac.in>
2023-08-23 21:03:45 +0530

    asset update

--- bus_pass/lib/app/modules/home/views/home_view.dart
+++ bus_pass/lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,6 @@
+import 'package:bus_pass/app/modules/home/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
